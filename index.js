d28caf3790da3c3aed8500c3c246892d2432056d
Megan King <mgnking12@gmail.com>
2016-07-16 11:25:57 -0500

    tables

--- index.js
+++ index.js
@@ -42,20 +42,20 @@ var waitList = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'home.html'));
 })
 
 
 
 // Make a reservation
 app.get('/reserve', function(req, res){
-    res.sendFile(path.join(__dirname, 'add.html'));
+    res.sendFile(path.join(__dirname, 'reserve.html'));
 })
 
 
 //View reservations and wait list
 app.get('/listView', function(req, res){
-    res.sendFile(path.join(__dirname, 'add.html'));
+    res.sendFile(path.join(__dirname, 'tables.html'));
 })
 
 
@@ -67,4 +67,24 @@ app.get('/listView', function(req, res){
 // =============================================================
 app.listen(PORT, function(){
     console.log('App listening on PORT ' + PORT);
+})
+
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get('/api/:customer?', function(req, res){
+    var chosen = req.params.customer;
+    if(chosen){
+        console.log(chosen);
+        for (var i=0; i <customer.length; i++){
+
+            if (chosen == customer[i].routeName){
+                res.json(customer[i]);
+                return;
+            }
+        }
+        res.json(false);
+    }
+    else{
+        res.json(customer);
+    }
 })
