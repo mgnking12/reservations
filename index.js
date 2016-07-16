8a217cb3a6d9805cdcdba0c2538ecdbcbe9b3338
Jack Walker <jackw2050@gmail.com>
2016-07-16 13:41:12 -0500

    added addWait

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // Star Wars Characters (DATA)
 // =============================================================
@@ -27,34 +27,32 @@ var customer = [
     }
 ]
 
-var waitList = [
-    {
-        customerID: "JohnSmith",
-        customerName: "John Smith",
-        phoneNumber: "55-555-5555",
-        customerEmail: "jos@gmail.com"
-    }
-]
+var waitList = [{
+    customerID: "JohnSmith",
+    customerName: "John Smith",
+    phoneNumber: "55-555-5555",
+    customerEmail: "jos@gmail.com"
+}]
 
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'home.html'));
 })
 
 
 
 // Make a reservation
-app.get('/reserve', function(req, res){
+app.get('/reserve', function(req, res) {
     res.sendFile(path.join(__dirname, 'reserve.html'));
 })
 
 
 //View reservations and wait list
-app.get('/tables', function(req, res){
+app.get('/tables', function(req, res) {
     res.sendFile(path.join(__dirname, 'tables.html'));
 })
 
@@ -62,42 +60,55 @@ app.get('/tables', function(req, res){
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function(){
+app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
 })
 
 
 
 
-app.get('/api/:customer?', function(req, res){
+app.get('/api/:customer?', function(req, res) {
     var chosen = req.params.customer;
-    if(chosen){
+    if (chosen) {
         console.log(chosen);
-        for (var i=0; i <customer.length; i++){
+        for (var i = 0; i < customer.length; i++) {
 
-            if (chosen == customer[i].routeName){
+            if (chosen == customer[i].routeName) {
                 res.json(customer[i]);
                 return;
             }
         }
         res.json(false);
-    }
-    else{
+    } else {
         res.json(customer);
     }
 })
 
 
 // Create New customer - takes in JSON input
-app.post('/api/new', function(req, res){
+app.post('/api/new', function(req, res) {
 
     var newCustomer = req.body;
-    console.log(newCustomer);
     newCustomer.routeName = newCustomer.customerName.replace(/\s+/g, '');
-
-    console.log(newCustomer);
-
     customer.push(newCustomer);
-
     res.json(newCustomer);
+})
+// Create New customer - takes in JSON input
+app.post('/api/newWait', function(req, res) {
+
+    var newWait = req.body;
+    newWait.routeName = newWait.customerName.replace(/\s+/g, '');
+    waitList.push(newWait);
+    res.json(newWait);
+})
+
+// Create New customer - takes in JSON input
+app.post('/api/clear', function(req, res) {
+
+    for (var i = customer.length - 1; i >= 0; i--) {
+        customer.pop();
+    }
+    res.json(customer);
+
+
 })
